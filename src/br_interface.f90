0f0f8a367336e40a9d14588346070a43e38caec3
Loïc Reynier <contact@loicreynier.fr>
2025-12-04 15:29:56 +0100

    chore: init repo with `sin`, `cos` & `exp`

---
+++ src/br_interface.f90
@@ -0,0 +1,66 @@
+module bitrep
+
+  use, intrinsic :: iso_c_binding, only: c_double
+  use iso_fortran_env, only: dp => real64
+  implicit none
+
+  private
+  public :: br_sin, br_cos, br_exp
+
+contains
+
+  elemental function br_sin(x)
+    !$acc routine seq
+    real(kind=dp), intent(in) :: x
+    real(kind=dp) :: br_sin
+
+    interface
+      pure function br_sin_c(x_c) bind(c, name="br_sin")
+        !$acc routine seq
+        import c_double
+        implicit none
+        real(kind=c_double) :: br_sin_c
+        real(kind=c_double), value, intent(in) :: x_c
+      end function br_sin_c
+    end interface
+
+    br_sin = br_sin_c(real(x, kind=c_double))
+  end function br_sin
+
+  elemental function br_cos(x)
+    !$acc routine seq
+    real(kind=dp), intent(in) :: x
+    real(kind=dp) :: br_cos
+
+    interface
+      pure function br_cos_c(x_c) bind(c, name="br_cos")
+        !$acc routine seq
+        import c_double
+        implicit none
+        real(kind=c_double) :: br_cos_c
+        real(kind=c_double), value, intent(in) :: x_c
+      end function br_cos_c
+    end interface
+
+    br_cos = br_cos_c(real(x, kind=c_double))
+  end function br_cos
+
+  elemental function br_exp(x)
+    !$acc routine seq
+    real(kind=dp), intent(in) :: x
+    real(kind=dp) :: br_exp
+
+    interface
+      pure function br_exp_c(x_c) bind(c, name="br_exp")
+        !$acc routine seq
+        import c_double
+        implicit none
+        real(kind=c_double) :: br_exp_c
+        real(kind=c_double), value, intent(in) :: x_c
+      end function br_exp_c
+    end interface
+
+    br_exp = br_exp_c(real(x, kind=c_double))
+  end function br_exp
+
+end module bitrep
